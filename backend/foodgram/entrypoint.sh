d70e6aba46928de2dda4ea848ab7dfdbd655e852
Nikita Kovalevskiy <koval701@gmail.com>
2022-01-15 00:22:15 +0700

    done

--- backend/foodgram/entrypoint.sh
+++ backend/foodgram/entrypoint.sh
@@ -1,6 +1,6 @@
 python manage.py collectstatic --noinput &&
-sleep 10 &&
+#sleep 10 &&
 python manage.py migrate --noinput &&
-sleep 10 &&
+#sleep 10 &&
 python manage.py loaddata dump.json &&
 gunicorn foodgram.wsgi:application --bind 0.0.0.0:8000
